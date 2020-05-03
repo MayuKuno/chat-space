016cecaf3bf4f98fcdef66d5d07f539e6d0ed43e
mayukuno <kunomayu1006@gmail.com>
2020-05-03 21:02:44 +0900

    Carrierwaveを導入

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,17 @@
 class MessagesController < ApplicationController
   def index
   end
+  def create
+    @message = Group.new(message_params)
+    if @message.save
+      redirect_to group_messages_path(group_id)
+    else
+      render :index
+    end
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(:content, :image)
+  end
 end
