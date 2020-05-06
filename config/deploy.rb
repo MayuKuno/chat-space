67b2424ea6e5324ac28db7f259c07552c884f073
mayukuno <kunomayu1006@gmail.com>
2020-05-06 22:01:43 +0900

    updated deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,21 +1,21 @@
-# config valid only for current version of Capistrano
-# capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
 lock '3.14.0'
 
-# Capistranoのログの表示に利用する
-set :application, 'chat-space''
+set :application, 'chat-space'
 
 set :repo_url,  'git@github.com:MayuKuno/chat-space.git'
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
-set :rbenv_ruby, '2.5.1' 
+set :rbenv_ruby, '2.5.1'
+
 
 set :ssh_options, auth_methods: ['publickey'],
                   keys: ['~/.ssh/Chatspace_Ninefs.pem'] 
 
+
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+
 
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
@@ -26,3 +26,4 @@ namespace :deploy do
     invoke 'unicorn:restart'
   end
 end
+
