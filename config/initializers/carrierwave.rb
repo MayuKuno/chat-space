50759bff631426f37a8c6d84d756c3e0a381a54d
mayukuno <kunomayu1006@gmail.com>
2020-05-08 19:55:16 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -14,6 +14,6 @@ CarrierWave.configure do |config|
     region: 'ap-northeast-1'
   }
 
-  config.fog_directory  = chatspace2020.ninefs
-  config.asset_host = https://s3-ap-northeast-1.amazonaws.com/chatspace2020.ninefs
+  config.fog_directory  = 'chatspace2020.ninefs'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/chatspace2020.ninefs'
 end
